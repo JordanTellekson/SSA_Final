3d91844dc3096c26190a4a2d65dd525465d0ebf0
Dalton Cournoyer <17461560@mstc.edu>
2026-03-21 04:29:20 -0500

    Add project files.

---
+++ SSA-Final/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using SSA_Final.Models;
+using System.Diagnostics;
+
+namespace SSA_Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
